c45778d06e408a1d8cd8b73126647fcd13a4fbdc
Vinh Le <vinh@vertics.co>
2020-08-25 06:53:30 +0300

    refactor: create setupNavController method

--- appstore_clone/ViewControllers/BaseTabBarController.swift
+++ appstore_clone/ViewControllers/BaseTabBarController.swift
@@ -15,28 +15,21 @@ class BaseTabBarController: UITabBarController {
     }
     
     fileprivate func setupViewControllers() {
-        let redViewController = UIViewController()
-        redViewController.view.backgroundColor = .white
-        redViewController.navigationItem.title = "Apps"
-        
-        let blueViewController = UIViewController()
-        blueViewController.view.backgroundColor = .white
-        blueViewController.navigationItem.title = "Search"
-        
-        // Setup navigation controller for each view
-        let redNavController = UINavigationController(rootViewController: redViewController)
-        let blueNavController = UINavigationController(rootViewController: blueViewController)
-        redNavController.navigationBar.prefersLargeTitles = true
-        blueNavController.navigationBar.prefersLargeTitles = true
-        // Setup tabBarItem
-        redNavController.tabBarItem.title = "Apps"
-        redNavController.tabBarItem.image = #imageLiteral(resourceName: "today_icon")
-        blueNavController.tabBarItem.title = "Search"
-        blueNavController.tabBarItem.image = #imageLiteral(resourceName: "search")
-        
         viewControllers = [
-            redNavController,
-            blueNavController
+            setupNavController(viewController: UIViewController(), title: "Today", image: #imageLiteral(resourceName: "today_icon")),
+            setupNavController(viewController: UIViewController(), title: "Apps", image: #imageLiteral(resourceName: "apps")),
+            setupNavController(viewController: UIViewController(), title: "Search", image: #imageLiteral(resourceName: "search"))
         ]
+    }
+    
+    fileprivate func setupNavController(viewController: UIViewController, title: String, image: UIImage) -> UINavigationController {
+        viewController.view.backgroundColor = .white
+        viewController.navigationItem.title = title
+        
+        let navViewController = UINavigationController(rootViewController: viewController)
+        navViewController.navigationBar.prefersLargeTitles = true
+        navViewController.tabBarItem.title = title
+        navViewController.tabBarItem.image = image
+        return navViewController
     }
 }
