490001da71c7cf379063530261775ad89ba9460d
Vinh Le <vinh@vertics.co>
2020-08-25 06:13:17 +0300

    feat: initial commit and remove storyboard

---
+++ appstore_clone/ViewControllers/BaseTabBarController.swift
@@ -0,0 +1,16 @@
+//
+//  BaseTabBarController.swift
+//  appstore_clone
+//
+//  Created by Vinh Le on 8/25/20.
+//  Copyright © 2020 Vinh Le. All rights reserved.
+//
+
+import UIKit
+
+class BaseTabBarController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
