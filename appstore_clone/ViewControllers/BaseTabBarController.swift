aaedfff4636cf0a50f02e6099d9150149087f640
Vinh Le <vinh@vertics.co>
2020-08-25 06:32:38 +0300

    feat: init navigation controller

--- appstore_clone/ViewControllers/BaseTabBarController.swift
+++ appstore_clone/ViewControllers/BaseTabBarController.swift
@@ -11,6 +11,32 @@ import UIKit
 class BaseTabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        setupViewControllers()
+    }
+    
+    fileprivate func setupViewControllers() {
+        let redViewController = UIViewController()
+        redViewController.view.backgroundColor = .white
+        redViewController.navigationItem.title = "Apps"
+        
+        let blueViewController = UIViewController()
+        blueViewController.view.backgroundColor = .white
+        blueViewController.navigationItem.title = "Search"
+        
+        // Setup navigation controller for each view
+        let redNavController = UINavigationController(rootViewController: redViewController)
+        let blueNavController = UINavigationController(rootViewController: blueViewController)
+        redNavController.navigationBar.prefersLargeTitles = true
+        blueNavController.navigationBar.prefersLargeTitles = true
+        // Setup tabBarItem
+        redNavController.tabBarItem.title = "Apps"
+        redNavController.tabBarItem.image = #imageLiteral(resourceName: "today_icon")
+        blueNavController.tabBarItem.title = "Search"
+        blueNavController.tabBarItem.image = #imageLiteral(resourceName: "search")
+        
+        viewControllers = [
+            redNavController,
+            blueNavController
+        ]
     }
 }
