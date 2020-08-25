e0aa1ab7591486a8894627b7a46c2296973780ea
Vinh Le <vinh@vertics.co>
2020-08-25 05:25:15 +0300

    Initial Commit

---
+++ appstore_clone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  appstore_clone
+//
+//  Created by Vinh Le on 8/25/20.
+//  Copyright © 2020 Vinh Le. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
