1e38de9c0c2a42080ed25f4a51cff7edf191a8c1
Vinh Le <vinh@vertics.co>
2020-08-26 07:05:10 +0300

    feat: add extension file

---
+++ appstore_clone/Extensions/Extensions.swift
@@ -0,0 +1,103 @@
+//
+//  Extensions.swift
+//  appstore_clone
+//
+//  Created by Vinh Le on 8/26/20.
+//  Copyright © 2020 Vinh Le. All rights reserved.
+//
+import UIKit
+
+extension UIView {
+    func anchor(top: NSLayoutYAxisAnchor?, leading: NSLayoutXAxisAnchor?, bottom: NSLayoutYAxisAnchor?, trailing: NSLayoutXAxisAnchor?, paddingTop: CGFloat = 0, paddingLeft: CGFloat = 0, paddingBottom: CGFloat = 0, paddingRight: CGFloat = 0, size: CGSize = .zero) {
+        self.translatesAutoresizingMaskIntoConstraints = false
+        if let top = top {
+            self.topAnchor.constraint(equalTo: top, constant: paddingTop).isActive = true
+        }
+        
+        if let leading = leading {
+            self.leadingAnchor.constraint(equalTo: leading, constant: paddingLeft).isActive = true
+        }
+        
+        if let bottom = bottom {
+            self.bottomAnchor.constraint(equalTo: bottom, constant: -paddingBottom).isActive = true
+        }
+        
+        if let trailing = trailing {
+            self.trailingAnchor.constraint(equalTo: trailing, constant: -paddingRight).isActive = true
+        }
+        
+        if size.width != 0 {
+            self.widthAnchor.constraint(equalToConstant: size.width).isActive = true
+        }
+        
+        if size.height != 0 {
+            self.heightAnchor.constraint(equalToConstant: size.height).isActive = true
+        }
+    }
+    
+    func fillSuperview(padding: UIEdgeInsets = .zero) {
+        translatesAutoresizingMaskIntoConstraints = false
+        if let superviewTopAnchor = superview?.topAnchor {
+            topAnchor.constraint(equalTo: superviewTopAnchor, constant: padding.top).isActive = true
+        }
+        
+        if let superviewBottomAnchor = superview?.bottomAnchor {
+            bottomAnchor.constraint(equalTo: superviewBottomAnchor, constant: -padding.bottom).isActive = true
+        }
+        
+        if let superviewLeadingAnchor = superview?.leadingAnchor {
+            leadingAnchor.constraint(equalTo: superviewLeadingAnchor, constant: padding.left).isActive = true
+        }
+        
+        if let superviewTrailingAnchor = superview?.trailingAnchor {
+            trailingAnchor.constraint(equalTo: superviewTrailingAnchor, constant: -padding.right).isActive = true
+        }
+    }
+}
+
+class AttributedText {
+    var text: String
+    var fontSize: CGFloat
+    var fontWeight: UIFont.Weight
+    var color: UIColor
+    
+    init(text: String, fontSize: CGFloat, fontWeight: UIFont.Weight, color: UIColor) {
+        self.text = text
+        self.fontSize = fontSize
+        self.fontWeight = fontWeight
+        self.color = color
+    }
+}
+
+extension UILabel {
+    func addAttributeText(primaryText: String, secondaryText: String) {
+        let attributedText = NSMutableAttributedString(string: primaryText, attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 17)])
+        attributedText.append(NSAttributedString(string: secondaryText, attributes: [NSAttributedString.Key.foregroundColor: UIColor.darkGray, NSAttributedString.Key.font: UIFont.systemFont(ofSize: 14)]))
+        self.attributedText = attributedText
+    }
+
+    //
+    // This should be used over the previous method
+    //
+    func appendAttributedText(firstText: AttributedText, secondText: AttributedText) {
+        let attributedText = NSMutableAttributedString(string: firstText.text, attributes: [
+            NSAttributedString.Key.font: UIFont.systemFont(ofSize: firstText.fontSize, weight: firstText.fontWeight),
+            NSAttributedString.Key.foregroundColor: firstText.color
+        ])
+        attributedText.append(NSAttributedString(string: secondText.text, attributes: [
+            NSAttributedString.Key.font: UIFont.systemFont(ofSize: secondText.fontSize, weight: secondText.fontWeight),
+            NSAttributedString.Key.foregroundColor: secondText.color
+        ]))
+        self.attributedText = attributedText
+    }
+}
+
+extension String {
+    func capitalizingFirstLetter() -> String {
+        return prefix(1).capitalized + dropFirst()
+    }
+
+    mutating func capitalizeFirstLetter() {
+        self = self.capitalizingFirstLetter()
+    }
+}
