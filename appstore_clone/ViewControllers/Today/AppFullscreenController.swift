39f6f3274b3e176da599b5ec2935b1a68f14f928
Vinh Le <vinh@vertics.co>
2020-10-08 06:22:46 +0300

    feat: setup floating blur view

--- appstore_clone/ViewControllers/Today/AppFullscreenController.swift
+++ appstore_clone/ViewControllers/Today/AppFullscreenController.swift
@@ -22,6 +22,36 @@ class AppFullScreenController: UIViewController, UITableViewDataSource, UITableV
         return button
     }()
     
+    let downloadButton: UIButton = {
+        let button = UIButton(title: "GET", cornerRadius: 16)
+        button.backgroundColor = .gray
+        button.titleLabel?.font = .systemFont(ofSize: 16)
+        button.setTitleColor(.white, for: .normal)
+        button.constrainSize(width: 60, height: 30)
+        return button
+    }()
+    
+    let appLogoView: UIImageView = {
+        let appLogoView = UIImageView()
+        appLogoView.constrainSize(width: 50, height: 50)
+        appLogoView.layer.cornerRadius = 12
+        appLogoView.clipsToBounds = true
+        return appLogoView
+    }()
+    
+    let floatingView: UIView = {
+        let view = UIView()
+        view.layer.cornerRadius = 16
+        view.clipsToBounds = true
+        view.constrainSize(width: 0, height: 80)
+        return view
+    }()
+    
+    let blurView: UIVisualEffectView = {
+        let view = UIVisualEffectView(effect: UIBlurEffect(style: .regular))
+        return view
+    }()
+    
     let tableView = UITableView(frame: .zero, style: .plain)
     
     override func viewDidLoad() {
@@ -30,6 +60,7 @@ class AppFullScreenController: UIViewController, UITableViewDataSource, UITableV
         view.clipsToBounds = true
         setupTableView()
         setupCloseButton()
+        setupFloatingView()
     }
     
     fileprivate func setupTableView() {
@@ -49,6 +80,42 @@ class AppFullScreenController: UIViewController, UITableViewDataSource, UITableV
     fileprivate func setupCloseButton() {
         view.addSubview(closeButton)
         closeButton.anchor(top: view.topAnchor, leading: nil, bottom: nil, trailing: view.trailingAnchor, paddingTop: 54, paddingRight: 24)
+    }
+    
+    // When we first launch the application, status bar frame height is 0
+    // so we need to update this new value once window changes
+    let statusBarHeight: CGFloat = {
+        var heightToReturn: CGFloat = 0.0
+             for window in UIApplication.shared.windows {
+                 if let height = window.windowScene?.statusBarManager?.statusBarFrame.height, height > heightToReturn {
+                     heightToReturn = height
+                 }
+             }
+        return heightToReturn
+    }()
+    fileprivate func setupFloatingView() {
+        view.addSubview(floatingView)
+        floatingView.anchor(top: nil, leading: view.leadingAnchor, bottom: view.bottomAnchor, trailing: view.trailingAnchor, paddingLeft: 16, paddingBottom: statusBarHeight, paddingRight: 16)
+        floatingView.addSubview(blurView)
+        blurView.fillSuperview()
+        
+        // Setup content
+        let label = UILabel(text: todayItem?.title ?? "", font: .boldSystemFont(ofSize: 18))
+        let description = UILabel(text: todayItem?.description ?? "", font: .systemFont(ofSize: 16))
+        appLogoView.image = todayItem?.image
+            
+        let stackView = UIStackView(arrangedSubviews: [
+            appLogoView,
+            VerticalStackView(arrangedSubViews: [
+                label, description
+            ]),
+            downloadButton
+        ])
+        stackView.alignment = .center
+        stackView.spacing = 12
+        
+        floatingView.addSubview(stackView)
+        stackView.fillSuperview(padding: .init(top: 12, left: 16, bottom: 12, right: 16))
     }
     
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
